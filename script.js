0af6674620bdbf91142a0eadecf8eace4d8ab456
alejo8903 <alejo890327@gmail.com>
2022-12-31 22:06:13 -0500

    agregando archivos

---
+++ script.js
@@ -0,0 +1,120 @@
+document.getElementById("respuesta").innerHTML="";
+
+const boton = document.querySelector("#encriptar");
+boton.addEventListener("click",function(evento) {
+    var respuesta= String(document.getElementById("respuesta").value);
+    var codigo = document.getElementById("codigo").value;
+    if (String(codigo)=="") {
+        alert("debe ingresar texto para encriptar")
+
+    }else{
+        var codigo2=String(codigo);
+        encriptar(codigo2);
+        document.getElementById("imagen").style.visibility="hidden";
+        document.getElementById("mensaje").style.visibility="hidden"; 
+    }
+   
+        
+
+})
+
+const boton1 = document.querySelector("#desencriptar");
+boton1.addEventListener("click",function(evento) {
+    var respuesta= String(document.getElementById("respuesta").value);
+    var codigo = document.getElementById("codigo").value;
+    if (String(codigo)=="") {
+        alert("debe ingresar texto para encriptar")
+
+    }else{
+        var codigo3=String(codigo);
+        desencriptar(codigo3);
+        document.getElementById("imagen").style.visibility="hidden";
+        document.getElementById("mensaje").style.visibility="hidden"; 
+    }
+   
+        
+
+})
+
+
+
+function desencriptar(palabra) {
+    var letra;
+    var desencriptado="";
+    var x= palabra.length
+    for(var i = 0;i<x;i++){
+        letra=palabra[i];
+        if (letra=="a" || letra=="e" || letra=="i" || letra=="o" || letra=="u"){
+            switch (letra) {
+                case "a":
+                    letra="a"
+                    i=i+1
+                    break;
+                case "e":
+                    letra="e"
+                    i=i+4
+                    break;
+                case "i":
+                    letra="i"
+                    i=i+3
+                    break;
+                case "o":
+                    letra="o"
+                    i=i+3
+                    break;
+                case "u":
+                    letra="u"
+                    i=i+3
+                    break;
+            
+            
+                default:
+                    break;
+            }
+        }
+
+        desencriptado=desencriptado+letra;
+    
+    }
+    document.getElementById("respuesta").innerHTML=desencriptado;
+
+    
+}
+
+function encriptar(palabra) {
+    var letra;
+    var encriptado="";
+    var x= palabra.length
+    for(var i = 0;i<x;i++){
+        letra=palabra[i];
+        if (letra=="a" || letra=="e" || letra=="i" || letra=="o" || letra=="u"){
+            switch (letra) {
+                case "a":
+                    letra="ai"
+                    break;
+                case "e":
+                    letra="enter"
+                    break;
+                case "i":
+                    letra="imes"
+                    break;
+                case "o":
+                    letra="ober"
+                    break;
+                case "u":
+                    letra="ufat"
+                    break;
+            
+            
+                default:
+                    break;
+            }
+        }
+
+        encriptado=encriptado+letra;
+    
+    }
+    document.getElementById("respuesta").innerHTML=encriptado;
+
+    
+}
